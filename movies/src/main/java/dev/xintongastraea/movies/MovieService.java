b2333cfde26a0a2a88150afeb81a7b7964ceea67
XintongLi1 <1720731919@qq.com>
2024-03-03 17:59:23 -0500

    Java Spring Boot backend tutorial

---
+++ movies/src/main/java/dev/xintongastraea/movies/MovieService.java
@@ -0,0 +1,25 @@
+package dev.xintongastraea.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired  // Instantiated by the framework
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
